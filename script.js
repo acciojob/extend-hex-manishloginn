a544a4b37626d5ac901047e1a1832d761d47e585
manishloginn <132683386+manishloginn@users.noreply.github.com>
2024-02-07 22:43:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
 const extendHex = (shortHex) => {
   // write your code here
+ 
+  let hexCode = ["#"],
+    isCapital = false;
+ 
+  shortHex = shortHex.replace("#", "");
+ 
+  for (let i = 0; i < shortHex.length; i++) {
+    if (
+      shortHex.charAt(i) === shortHex.charAt(i).toUpperCase() &&
+      shortHex.charAt(i) !== shortHex.charAt(i).toLowerCase()
+    ) {
+      isCapital = true;
+      break;
+    }
+  }
+ 
+  for (let i = 0; i < shortHex.length; i++) {
+    if (isCapital) {
+      hexCode.push(shortHex.charAt(i).toUpperCase());
+      hexCode.push(shortHex.charAt(i).toUpperCase());
+    } else {
+      hexCode.push(shortHex.charAt(i));
+      hexCode.push(shortHex.charAt(i));
+    }
+  }
+ 
+  return hexCode.join("");
 };
-
+ 
 // Do not change the code below.
 const shortHex = prompt("Enter Short Hex.");
 alert(extendHex(shortHex));
